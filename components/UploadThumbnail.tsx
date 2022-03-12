46ab840c2c0fb838cf3d8a079f3fe2eb89baf965
Verena Severina <verenaseverina@Apples-MacBook-Pro.local>
2022-03-12 13:01:16 +0800

    add upload function

--- components/UploadThumbnail.tsx
+++ components/UploadThumbnail.tsx
@@ -1,24 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 type Props = {
   label: string;
-  value?: File;
+  value?: string;
   required?: boolean;
-  onChange?: (val) => void;
+  onChange?: (url: string, file: File) => void;
 }
+
+function getBase64 (file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      resolve(reader.result as string);
+    };
+    reader.onerror = function (error) {
+      reject(error);
+    };
+  })
+}
+
 
 const UploadThumbnail = ({
     required = true,
     ...props
-  }: Props) => (
-  <div>
-    <p>{props.label}</p>
+  }: Props) => {
+  const [file, setFile] = useState<File | null>(null);
+  const [url, setUrl] = useState<string>('');
+
+  const onChangeFile = useCallback(async (e: any) => {
+    const selectedFile = e.target.files[0]
+    const urlFile = await getBase64(selectedFile)
+    if (props.onChange) {
+      props.onChange(urlFile, 
+        selectedFile);
+      setFile(selectedFile)
+    } else {
+      setFile(selectedFile)
+      setUrl(urlFile)
+    }
+  }, [props.onChange])
+
+  const removeFile = () => {
+    props.onChange?.('', null);
+    setUrl('');
+    setFile(null);
+  }
+
+  useEffect(() => {
+    setUrl(props.value)
+  }, [props.value]);
+
+  return (
     <div>
-      <span>Add Image</span>
-      <input type="file"
-        accept="image/png, image/jpeg, image/jpg"></input>
+      <p>{props.label}</p>
+      {
+        url ? (
+          <div>
+            <img src={url} />
+            <button onClick={() => removeFile()}>Delete</button>
+          </div>
+        ) : (
+          <div>
+            <span>Add Image</span>
+            <input 
+              type="file"
+              onChange={onChangeFile}
+              accept="image/png, image/jpeg, image/jpg">
+            </input>
+          </div>
+        )
+      }
     </div>
-  </div>
-)
+  )
+}
 
 export default UploadThumbnail
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,29 @@
+import React, { useState } from 'react'
 import Layout from '../components/Layout'
 import ImagePreview from '../components/ImagePreview'
 import UploadThumbnail from '../components/UploadThumbnail'
 
-const IndexPage = () => (
-  <Layout>
-    <div>
-      <ImagePreview />
-    </div>
-    <div>
-      <UploadThumbnail 
-        label="Thumbnail Image"
-      />
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  const [imageUrl, setImageUrl] = useState<string>('');
+  
+  return (
+    <Layout>
+      <div className="grid grid-cols-2 divide-x">
+        <div className="flex justify-center">
+          <ImagePreview
+            imgValue={imageUrl}
+          />
+        </div>
+        <div className="flex justify-center">
+          <UploadThumbnail 
+            label="Thumbnail Image"
+            value={imageUrl}
+            onChange={(url) => setImageUrl(url)}
+          />
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage

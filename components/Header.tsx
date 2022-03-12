e1abdb47728afcaa3f65014e6588e7042ce3f9fb
Verena Severina <verenaseverina@Apples-MacBook-Pro.local>
2022-03-12 14:16:22 +0800

    add header user photo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Example() {
   return (  
   <div className="mx-auto flex justify-between bg-black">
-    <div className="flex justify-start items-center px-10 py-6 space-x-36 text-white font-medium">
+    <div className="flex justify-start items-center px-10 py-1 space-x-36 text-white font-medium">
       <a href="#">
         <span className="">LOGO</span>
       </a>
@@ -20,8 +20,11 @@ export default function Example() {
         </a>
       </div>
     </div>
-    <div className="flex justify-end px-10 py-6">
-      USER
+    <div className="flex justify-end px-10 py-1">
+      <div className="relative">
+        <img className="w-12" src="/profile-pic.png" />
+        <img className="absolute top-2 right-2.5 w-2" src="/red-dot.svg" />
+      </div>
     </div>
   </div>
   )
